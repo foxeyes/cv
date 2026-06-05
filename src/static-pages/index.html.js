7853afd5cdadd17a83051ddddfd1a006321a57cf
foxeyes <foxrandom.com@gmail.com>
2026-06-05 10:42:43 -0300

    chore: minor update

--- src/static-pages/index.html.js
+++ src/static-pages/index.html.js
@@ -2,6 +2,6 @@ import { getPage } from './getPage.js';
 
 export default await getPage({
   TITLE: 'Alex Matiasevich',
-  HEADER_CONTENT: 'Alex Matiasevich',
+  HEADER_CONTENT: 'Alex Matiasevich CV',
   MD_URL: './src/static-pages/cv.md',
 });
